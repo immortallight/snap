ec07462b32346a4cf3bec02b4323128e3e408613
immortallight <raythymson2018@gmail.com>
2022-06-23 22:49:11 +0100

    mark-ups

--- index.js
+++ index.js
@@ -1,14 +1,12 @@
-// document.querySelectorAll(".question").forEach((question) =>question.addEventListener("click", () => {
- 
-//     if(question.parentNode.classList.contains("active")){
+// alert  ("hello")
+// document.querySelectorAll("new").addEventListener("click", myToggler);
+// function myToggler(){
+//     document.querySelectorAll(dropAnswer).forEach.toggle("show")
+// }
 
-//         question.parentNode.classlist.toggle("active")
-//  }
-//  else{
-//      document.querySelectorAll(".question").forEach(question => question.parentNode.classList.remove("active"))
 
-//      question.parentNode.classList.add("active")
-//  }
- 
 
-// }))
+document.getElementsByClassName("featuresDrop-down").addEventListener("click", myFeature);
+function myFeature(){
+    getElementsByClassName("feature-drop-answer").style.display = "block"
+}
